86e7e6392138708c64dbab7d1a9bfb8790158163
Josell Vibar <vibarjosell@gmail.com>
2022-10-28 20:23:44 +0800

    add index.php

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My First Php File</title>
+    <title>Homepage</title>
 </head>
 <body>
     <h1>My First Php</h1>
     <?php 
-        echo"My first Php Script"
+        echo"This is my Homepage";
     ?>
+
 </body>
 </html>
