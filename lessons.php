b1f7053c4e48daca6c3fd2a9f723996cf8647791
Josell Vibar <vibarjosell@gmail.com>
2022-11-03 19:25:49 +0800

    add lessons.php

---
+++ lessons.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro to php</title>
+</head>
+<body>
+    <h1>Intro to Php</h1>
+    <h2>10 primitive types</h2>
+    <h2>Scalar</h2>
+    <ul>
+        <li>String</li>
+        <li>Integer</li>
+        <li>Float</li>
+        <li>Boolean</li>
+    </ul>
+    
+    <h2>Compound</h2>
+    <ul>
+        <li>Array</li>
+        <li>Object</li>
+        <li>Callable</li>
+        <li>Iterable</li>
+    </ul>
+
+    <h2>Special</h2>
+    <ul>
+        <li>Resource</li>
+        <li>Null</li>
+    </ul>
+    <h2>Pseudo Types</h2>
+    <hr />
+
+    <h2>Strings</h2>
+    <h3>Double Quotes</h3>
+    <p>"This is a string data type in Php"</p>
+    <h3>Single Quotes</h3>
+</body>
+</html>
